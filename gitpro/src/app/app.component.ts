3e89cfeb56490b5fec20c70adba7817ba6e72b94
NivedhaNivi3 <nivedhaknivedha@gmail.com>
2021-08-29 21:58:03 +0530

    nivi-commit4

--- gitpro/src/app/app.component.ts
+++ gitpro/src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'gitpro';
    nivi="Interpolation";
+   visible=false;
+   show(){
+     console.log('successfully done');
+   }
 }
